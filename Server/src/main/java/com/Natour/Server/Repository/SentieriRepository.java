04dd38190b6210ff847fc57146912a567394ceee
Fciccare <76161641+Fciccare@users.noreply.github.com>
2023-02-05 16:47:31 +0100

    clean directory

--- Server/src/main/java/com/Natour/Server/Repository/SentieriRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Natour.Server.Repository;
-
-import com.Natour.Server.Entity.Sentieri;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SentieriRepository extends CrudRepository<Sentieri,Long> {
-}
